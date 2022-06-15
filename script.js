fb4eff042ce60eecd5c66db2a4d86e24dbc15db5
Gabriel Chagas <gabrielserq@outlook.com>
2022-06-15 10:36:13 -0300

    chore: creating itens array

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const controls = document.querySelector('.controls-container');
 
 let cards, interval, firstCard = false,
 secondCard = false;
+
+// Itens Array
+const items = [
+  { name: "bee", image: "bee.png" },
+  { name: "crocodile", image: "crocodile.png" },
+  { name: "macaw", image: "macaw.png" },
+  { name: "gorilla", image: "gorilla.png" },
+  { name: "tiger", image: "tiger.png" },
+  { name: "monkey", image: "monkey.png" },
+  { name: "chameleon", image: "chameleon.png" },
+  { name: "piranha", image: "piranha.png" },
+  { name: "anaconda", image: "anaconda.png" },
+  { name: "sloth", image: "sloth.png" },
+  { name: "cockatoo", image: "cockatoo.png" },
+  { name: "toucan", image: "toucan.png" },
+]
